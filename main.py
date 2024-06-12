987f056b1a6031958bf2ba17d82dacaf4a33015d
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-06-11 18:51:44 -0700

    added user search and fixed ranks for mangas

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ top100_manga = response.text
 soup = BeautifulSoup(top100_manga, "html.parser")
 
 ranking_rows = soup.find_all('tr', class_='ranking-list')
+hardcoded_rank = 0
+mangas = []
+users_favorite_manga = input("Here are the top 50 manga titles on mal. Type in your favorite manga to check if it in there.")
+
+
 for row in ranking_rows:
     # Find the rank
     ranked = ''
     rank_td = row.find('td', class_='rank ac')
-
+    hardcoded_rank += 1
     if rank_td:
-        rank_span = rank_td.find('span', class_=f'lightLink top-anime-rank-text rank{x}')
+        rank_span = rank_td.find('span', class_=f'lightLink top-anime-rank-text rank')
         if rank_span:
             placement = rank_span.text.strip()
             ranked = placement
@@ -27,4 +32,9 @@ for row in ranking_rows:
         if title:
             manga_title = title.text.strip()
             the_title = manga_title
-    print(f"Rank: {ranked} Title: {the_title}")
+            mangas.append(the_title)
+    print(f"Rank: {hardcoded_rank} Title: {the_title}")
+
+if users_favorite_manga in mangas:
+    user_rank = mangas.index(users_favorite_manga) + 1
+    print(f"Your favorite manga {users_favorite_manga} is in the current top 50 at {user_rank}!")
